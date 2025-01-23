a8cc0428ecca212416bcd4dace7f7731b6199a40
豊田 真吾 <toyodasinngo@gmail.com>
2025-01-23 15:17:54 +0900

    アプリの動作確認

--- watchOS-motion-writer WatchKit Extension/ComplicationController.swift
+++ watchOS-motion-writer WatchKit Extension/ComplicationController.swift
@@ -30,7 +30,6 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
         // Call the handler with the last entry date you can currently provide or nil if you can't support future timelines
-        handler(nil)
     }
     
     func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
